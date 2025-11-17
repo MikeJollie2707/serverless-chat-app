79523ce717997456d5d1d3e7786564eecd7f29f0
MikeJollie2707 <bachngo@yahoo.com>
2025-11-17 11:48:44 -0800

    Handle unknown connectionID

--- lambda/broadcast_message.py
+++ lambda/broadcast_message.py
@@ -2,14 +2,16 @@ import json
 import boto3
 import os
 
+from botocore.exceptions import ClientError
+
 def broadcast_message(event, context):
     api_gw = boto3.client(
         'apigatewaymanagementapi', 
-        region_name="us-west-1", 
+        region_name="us-west-1", # TODO: turn this to env
         endpoint_url=os.getenv("apigw_https_url"))
     print(event)
 
-    dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
+    dynamodb = boto3.resource('dynamodb')
     connectionTable = dynamodb.Table(os.getenv("connectionTableName"))
     response = connectionTable.scan()
     connections = response.get("Items", [])
@@ -22,14 +24,19 @@ def broadcast_message(event, context):
         # author, timestamp, etc.
         # print(body['MessageAttributes'])
 
-        # NOTE: May need to store region of a connection ID
-        # cuz if user connect from different gateways, that connectionID
-        # is not usable on the same api_gw
+        # NOTE: Maybe store a region field in dynamo so this loop is slightly more efficient?
         for connection in connections:
-            api_gw.post_to_connection(
-                ConnectionId=connection["connectionId"], 
-                Data=body['Message'],
-            )
+            try:
+                api_gw.post_to_connection(
+                    ConnectionId=connection["connectionId"], 
+                    Data=body['Message'],
+                )
+            except ClientError as e:
+                error_code = e.response['Error']['Code']
+                if error_code in ("GoneException", "Gone"):
+                    print(f"Connection {connection} is in different region. Skipped.")
+                else:
+                    print(e)
 
     
     return {
