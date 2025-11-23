0d431b1ed5ea880d383aef325c7d8fa0931da700
MikeJollie2707 <bachngo@yahoo.com>
2025-11-22 21:17:39 -0800

    Finalize terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "region1" {
   description = "AWS region to deploy resources into"
   type        = string
-  default     = "us-west-2"
+  default     = "us-west-1"
 }
 
 variable "region2" {
   description = "AWS region to deploy resources into"
   type        = string
-  default     = "us-east-2"
+  default     = "us-west-2"
 }
 
 variable "lambda_runtime" {
   description = "Lambda runtime to use for Python functions"
   type        = string
-  default     = "python3.11"
+  default     = "python3.12"
 }
 
 variable "stage" {
   description = "API Gateway stage name"
   type        = string
-  default     = "dev"
+  default     = "prod"
 }
